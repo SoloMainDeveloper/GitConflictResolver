fa8c30226af586943c5df687b9a96684b165ef85
Михаил Соломеин <solomein-m@mail.ru>
2025-09-19 13:05:58 +0500

    Удалил лишний конструктор

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,10 +1,6 @@
 package calculator;
 
 public class Calculator {
-    public Calculator(String operation) {
-        System.out.println(operation);
-    }
-    
     public Calculator(String owner){
         System.out.println("Hello. Im calculator. My owner is " + owner);
     }
