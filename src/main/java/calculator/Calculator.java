fbe8712a5b7799d603582e82aa1a096c5181fb3b
Михаил Соломеин <solomein-m@mail.ru>
2025-09-19 13:06:19 +0500

    Добавил метод суммы в Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public Calculator(String owner){
         System.out.println("Hello. Im calculator. My owner is " + owner);
     }
+
+    public int sum(int x, int y){
+        return x + y;
+    }
 }
